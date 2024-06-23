068e88f309481d31c668d7afb9ca14e246f4fe2f
Simon Skorokhodov <skorohodovsemen@gmail.com>
2024-06-23 20:02:22 +0200

    Bind all the addreses resolving to the redirect URI host

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,91 @@ use tracing::{debug, error, info, trace, warn};
 
 mod config;
 
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct OidcEndpoints {
+    // "https://server.example.com"
+    pub issuer: Option<String>,
+
+    // "https://server.example.com/connect/authorize"
+    pub authorization_endpoint: Option<String>,
+
+    // "https://server.example.com/connect/token"
+    pub token_endpoint: Option<String>,
+
+    // ["client_secret_basic", "private_key_jwt"]
+    pub token_endpoint_auth_methods_supported: Option<Vec<String>>,
+
+    // ["RS256", "ES256"]
+    pub token_endpoint_auth_signing_alg_values_supported: Option<Vec<String>>,
+
+    // "https://server.example.com/connect/userinfo"
+    pub userinfo_endpoint: Option<String>,
+
+    // "https://server.example.com/connect/check_session"
+    pub check_session_iframe: Option<String>,
+
+    // "https://server.example.com/connect/end_session"
+    pub end_session_endpoint: Option<String>,
+
+    // "https://server.example.com/jwks.json"
+    pub jwks_uri: Option<String>,
+
+    // "https://server.example.com/connect/register"
+    pub registration_endpoint: Option<String>,
+
+    // ["openid", "profile", "email", "address", phone, "offline_access"]
+    pub scopes_supported: Option<Vec<String>>,
+
+    // ["code", "code id_token", "id_token", "id_token token"]
+    pub response_types_supported: Option<Vec<String>>,
+
+    // ["urn:mace:incommon:iap:silver", urnmace:incommon:iap:bronze"]
+    pub acr_values_supported: Option<Vec<String>>,
+
+    // ["public", "pairwise"]
+    pub subject_types_supported: Option<Vec<String>>,
+
+    // ["RS256", "ES256", "HS256"]
+    pub userinfo_signing_alg_values_supported: Option<Vec<String>>,
+
+    // ["RSA-OAEP-256", "A128KW"]
+    pub userinfo_encryption_alg_values_supported: Option<Vec<String>>,
+
+    // ["A128CBC-HS256", "A128GCM"]
+    pub userinfo_encryption_enc_values_supported: Option<Vec<String>>,
+
+    // ["RS256", "ES256", "HS256"]
+    pub id_token_signing_alg_values_supported: Option<Vec<String>>,
+
+    // ["RSA-OAEP-256", "A128KW"]
+    pub id_token_encryption_alg_values_supported: Option<Vec<String>>,
+
+    // ["A128CBC-HS256", "A128GCM"]
+    pub id_token_encryption_enc_values_supported: Option<Vec<String>>,
+
+    // ["none", "RS256", "ES256"]
+    pub request_object_signing_alg_values_supported: Option<Vec<String>>,
+
+    // ["page", "popup"]
+    pub display_values_supported: Option<Vec<String>>,
+
+    // ["normal", "distributed"]
+    pub claim_types_supported: Option<Vec<String>>,
+
+    // ["sub", "iss", "auth_time", "acr", name, "given_name", "family_name", "nickname", profile, "picture", "website", email, "email_verified", "locale", "zoneinfo", http//example.info/claims/groups"]
+    pub claims_supported: Option<Vec<String>>,
+
+    // true
+    pub claims_parameter_supported: Option<bool>,
+
+    // "http://server.example.com/connect/service_documentation.html"
+    pub service_documentation: Option<String>,
+
+    // ["en-US", "en-GB", "en-CA", "fr-FR", "fr-CA"]
+    pub ui_locales_supported: Option<Vec<String>>,
+}
+
 async fn handle_request(
     endpoints: &OidcEndpoints,
     request: Request<hyper::body::Incoming>,
@@ -210,103 +295,19 @@ fn default_port(uri: &Uri) -> u16 {
     }
 }
 
-fn http_uri_socket_addr(uri: &Uri) -> Result<SocketAddr> {
+fn http_uri_socket_addrs(uri: &Uri) -> Result<Vec<SocketAddr>> {
     let port = uri.port_u16().unwrap_or_else(|| default_port(uri));
     let host = uri.host().unwrap_or("127.0.0.1");
     let host_port = host.to_string() + ":" + &port.to_string();
-    if let Some(addr) = host_port.to_socket_addrs()?.next() {
-        Ok(addr)
-    } else {
+    let addrs: Vec<_> = host_port.to_socket_addrs()?.collect();
+    if addrs.is_empty() {
         Err(anyhow::anyhow!(
             "cannot resolve socket address {}",
             host_port
         ))
+    } else {
+        Ok(addrs)
     }
-}
-
-#[derive(Deserialize, Debug)]
-#[allow(dead_code)]
-struct OidcEndpoints {
-    // "https://server.example.com"
-    pub issuer: Option<String>,
-
-    // "https://server.example.com/connect/authorize"
-    pub authorization_endpoint: Option<String>,
-
-    // "https://server.example.com/connect/token"
-    pub token_endpoint: Option<String>,
-
-    // ["client_secret_basic", "private_key_jwt"]
-    pub token_endpoint_auth_methods_supported: Option<Vec<String>>,
-
-    // ["RS256", "ES256"]
-    pub token_endpoint_auth_signing_alg_values_supported: Option<Vec<String>>,
-
-    // "https://server.example.com/connect/userinfo"
-    pub userinfo_endpoint: Option<String>,
-
-    // "https://server.example.com/connect/check_session"
-    pub check_session_iframe: Option<String>,
-
-    // "https://server.example.com/connect/end_session"
-    pub end_session_endpoint: Option<String>,
-
-    // "https://server.example.com/jwks.json"
-    pub jwks_uri: Option<String>,
-
-    // "https://server.example.com/connect/register"
-    pub registration_endpoint: Option<String>,
-
-    // ["openid", "profile", "email", "address", phone, "offline_access"]
-    pub scopes_supported: Option<Vec<String>>,
-
-    // ["code", "code id_token", "id_token", "id_token token"]
-    pub response_types_supported: Option<Vec<String>>,
-
-    // ["urn:mace:incommon:iap:silver", urnmace:incommon:iap:bronze"]
-    pub acr_values_supported: Option<Vec<String>>,
-
-    // ["public", "pairwise"]
-    pub subject_types_supported: Option<Vec<String>>,
-
-    // ["RS256", "ES256", "HS256"]
-    pub userinfo_signing_alg_values_supported: Option<Vec<String>>,
-
-    // ["RSA-OAEP-256", "A128KW"]
-    pub userinfo_encryption_alg_values_supported: Option<Vec<String>>,
-
-    // ["A128CBC-HS256", "A128GCM"]
-    pub userinfo_encryption_enc_values_supported: Option<Vec<String>>,
-
-    // ["RS256", "ES256", "HS256"]
-    pub id_token_signing_alg_values_supported: Option<Vec<String>>,
-
-    // ["RSA-OAEP-256", "A128KW"]
-    pub id_token_encryption_alg_values_supported: Option<Vec<String>>,
-
-    // ["A128CBC-HS256", "A128GCM"]
-    pub id_token_encryption_enc_values_supported: Option<Vec<String>>,
-
-    // ["none", "RS256", "ES256"]
-    pub request_object_signing_alg_values_supported: Option<Vec<String>>,
-
-    // ["page", "popup"]
-    pub display_values_supported: Option<Vec<String>>,
-
-    // ["normal", "distributed"]
-    pub claim_types_supported: Option<Vec<String>>,
-
-    // ["sub", "iss", "auth_time", "acr", name, "given_name", "family_name", "nickname", profile, "picture", "website", email, "email_verified", "locale", "zoneinfo", http//example.info/claims/groups"]
-    pub claims_supported: Option<Vec<String>>,
-
-    // true
-    pub claims_parameter_supported: Option<bool>,
-
-    // "http://server.example.com/connect/service_documentation.html"
-    pub service_documentation: Option<String>,
-
-    // ["en-US", "en-GB", "en-CA", "fr-FR", "fr-CA"]
-    pub ui_locales_supported: Option<Vec<String>>,
 }
 
 async fn discover_oidc_endpoints() -> Result<OidcEndpoints> {
@@ -350,8 +351,8 @@ async fn main() -> Result<()> {
 
     setup_logging(&config);
 
-    let addr = http_uri_socket_addr(&config.redirect_uri)?;
-    let listener = TcpListener::bind(addr).await?;
+    let addrs = http_uri_socket_addrs(&config.redirect_uri)?;
+    let listener = TcpListener::bind(addrs.as_slice()).await?;
 
     let endpoints = discover_oidc_endpoints().await?;
 
